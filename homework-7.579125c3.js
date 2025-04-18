bdba8faf61196719324cbe04f65497dcc4c1fa72
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-04-18 21:19:16 +0100

    Updates

--- homework-7.579125c3.js
+++ homework-7.579125c3.js
@@ -1,2 +1,5 @@
+const bookmarkInput = document.querySelector('#bookmarkInput');
+const addBookmarkBtn = document.querySelector('#addBookmarkBtn');
+const bookmarkList = document.querySelector('#bookmarkList');
 
 //# sourceMappingURL=homework-7.579125c3.js.map
---
+++ homework-7.bad2607c.js
@@ -0,0 +1,2 @@
+let bookmarkInput=document.querySelector("#bookmarkInput"),addBookmarkBtn=document.querySelector("#addBookmarkBtn"),bookmarkList=document.querySelector("#bookmarkList");
+//# sourceMappingURL=homework-7.bad2607c.js.map
